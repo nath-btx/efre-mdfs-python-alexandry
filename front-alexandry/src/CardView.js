4005d4c09c48b12a3beaa209d2118a962bfb00ec
valdofal <v.dofal@gmail.com>
2021-09-24 12:46:53 +0200

    d

--- front-alexandry/src/CardView.js
+++ front-alexandry/src/CardView.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-
+import DotLoader from "react-spinners/DotLoader"
 export default function CardView(){
 
 
@@ -7,19 +7,34 @@ export default function CardView(){
     const [books, setBooks] = useState([])
     useEffect(() => {
         getBooks()
-        console.log(books)
-    })
-    let getBooks = () =>{
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
+
+    const getBooks = async () => {
         fetch('http://localhost:8000/api/getBooks').then(
             response => response.json()).then(
                 resData => {
-                    console.log(resData)
+                    setBooks(resData)
                 }
             
         )
     }
     return(
-        <p>hello</p>
+        <div className="">
+            {books === [] ? (
+                <div>
+                </div>
+            ) : (
+                <div>
+                    {books.map((book, index) => 
+                        <div className="">
+                        </div>
+                    )}        
+                </div>
+
+            )}
+        </div>
+
     )
 
 }
